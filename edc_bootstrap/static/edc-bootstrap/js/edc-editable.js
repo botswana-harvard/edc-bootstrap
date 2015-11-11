08bd35341ba5d05787c4283a427ae69a9aefae47
erikvw <ew2789@gmail.com>
2015-11-11 19:53:32 +0200

    format js

---
+++ edc_bootstrap/static/edc-bootstrap/js/edc-editable.js
@@ -0,0 +1,21 @@
+
+/* initialize the datatable manually */
+$.fn.editable.defaults.mode = 'popup';
+datatableview.auto_initialize = false;
+$(function() {
+    var xeditable_options = {
+        error: function(data) {
+            /* actions on validation error (or ajax error) */
+            var msg = '';
+            if(data.errors) { //validation error
+                $.each(data.errors, function(k, v) { msg += k+": "+v+"<br>"; });  
+            } else if(data.responseText) { //ajax error, get form_errors text
+            	$.each(jQuery.parseJSON(data.responseText).form_errors, function(k, v) { msg += v+"<br>"; });	
+            	$('.editable-error-block').html(msg); 
+            }
+        },
+    };
+    datatableview.initialize($('.datatable'), {
+    	fnRowCallback: datatableview.make_xeditable(xeditable_options),
+    });
+});
